723a2b34e2bf6b4fef5d29c8e48ffd6e0b0d8ec1
fatimahK <fatimah.kamal@ontariotechu.net>
2025-11-23 18:13:02 -0500

    made header using bootstrap

--- books/routes/index.js
+++ books/routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Readist' });
+  res.render('index', { title: 'Readist' });
 });
 
-router.get('/', function(req, res, next) {
-  res.render('add', { title: 'Add Book' });
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'add' });
 });
 
-router.get('/', function(req, res, next) {
-  res.render('books', { title: 'My Books' });
+router.get('/books', function(req, res, next) {
+  res.render('books', { title: 'books' });
 });
 
 module.exports = router;
